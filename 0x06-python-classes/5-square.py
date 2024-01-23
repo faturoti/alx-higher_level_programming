ccc3f998c5e0212c6866b7ac7163d54bf4637e17
faturoti <abfat049@gmail.com>
2024-01-23 15:38:47 -0800

    Quest 1,2,3

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -6,7 +6,7 @@
 class Square:
     """Represent a square."""
 
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """Initialize a new Square.
 
         Args:
@@ -22,7 +22,7 @@ class Square:
     @size.setter
     def size(self, value):
         if not isinstance(size, int):
-            raise TypeError("size must be an integer");
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
 
