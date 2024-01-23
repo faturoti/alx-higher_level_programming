a32be11b19785ffebc809f1fb6640177a393ac48
faturoti <abfat049@gmail.com>
2024-01-23 15:22:29 -0800

    Quest 1,2,3

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,10 +6,8 @@
 class Square:
     """Represent a square."""
 
-
     def __init__(self, size):
         """Initialize a new Square.
-
 
         Args:
         size (int): The size of the new square.
