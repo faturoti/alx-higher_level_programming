b82da31eabbb3d67b10c1a8abcc604dafff3bb20
faturoti <abfat049@gmail.com>
2024-04-11 10:56:47 -0700

    Question 2

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const num = Math.floor(Number(process.argv[2]));
+if(isNaN(num)){
+	console.log('Missing size');
+}else{
+	for (let r = 0; r < num; r++) {
+		let row = '';
+		for(let n = 0; n < num; n++){
+			row += 'X';
+		}
+		console.log(row);
+	}
+}
