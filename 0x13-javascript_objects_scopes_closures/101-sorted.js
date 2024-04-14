1795dc4c22308579cfcfe8099e53946e269c4968
faturoti <abfat049@gmail.com>
2024-04-14 12:24:10 -0700

    Question 2

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,12 +3,13 @@ const dictInput = require('./101-data').dict;
 const outDict = {};
 
 for (const key in dictInput){
-	const occur = dictInput[key];
-	outDict[occur] = [];
-	for (const keys in dictInput) {
-		if (dictInput[keys] === occur) {
-			outDict[occur].push(keys);
-		}
-	}
+  const occur = dictInput[key];
+  outDict[occur] = [];
+  for (const keys in dictInput) {
+    if (dictInput[keys] === occur) {
+      outDict[occur].push(keys);
+    }
+  }
 }
+
 console.log(outDict);
