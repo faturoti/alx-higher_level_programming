750fb5e2a34d10cb4d07dffd5c75bee9be2305a8
faturoti <abfat049@gmail.com>
2024-04-20 12:53:15 -0700

    Question 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+The code shows the list of entries in state
+table in database(hbtn_0e_0_usa) .
+"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Tries to access the tables in the database 
+    and creates the connection first
+    """
+    db = MySQLdb.connect(host="localhost",
+                            user = argv[1],
+                            port = 3306,
+                            passwd = argv[2],
+                            db = argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+                    WHERE name LIKE BINARY 'N%' \
+                    ORDER BY states.id ASC")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
