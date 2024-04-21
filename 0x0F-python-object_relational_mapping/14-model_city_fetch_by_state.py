568b568e5556b0498aa3dc01b9afe3a30fcc3d58
faturoti <abfat049@gmail.com>
2024-04-21 08:01:28 -0700

    Question 0

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -7,18 +7,18 @@ from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-from mode_city import City
+from model_city import City
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     """
     This gets datafrom cities and states table
     in the database
     """
-    db_uri = "mysql+mysqldb://{}:{}@localhost:3306/{}"
-                .format(argv[1], argv[2], argv[3])
+    db_uri = "mysql+mysqldb://{}:{}@localhost:3306/{}".format
+                                                        (argv[1], argv[2], argv[3])
 
     engine = create_engine(db_uri)
-    Session = sessionmaker(bind = engine)
+    Session = sessionmaker(bind=engine)
 
     session = Session()
 
