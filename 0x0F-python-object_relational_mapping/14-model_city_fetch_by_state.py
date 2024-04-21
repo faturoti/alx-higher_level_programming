f743a65b99d7d667ecbbdd3256f525b2d5d0c9bd
faturoti <abfat049@gmail.com>
2024-04-21 08:10:03 -0700

    Question 0

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -14,8 +14,7 @@ if __name__ == "__main__":
     This gets datafrom cities and states table
     in the database
     """
-    db_uri = "mysql+mysqldb://{}:{}@localhost:3306/{}".format
-                                                        (argv[1], argv[2], argv[3])
+    db_uri = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3])
 
     engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
