4ce73d14f4cd08d04c6a3806252734ce7185e932
faturoti <abfat049@gmail.com>
2024-04-20 21:09:54 -0700

    Question 1

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+to fetch all the states from the 
+State table in database
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    This accesses the database vis localhost 
+    username and passward @ port 3306
+    """
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri);
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    inst = session.query(State).order_by(State.id).first()
+
+    if inst is None:
+        print('Nothing')
+    else:
+        print('{0}:{1}'.format(inst.id, inst.name))
